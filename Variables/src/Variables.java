797de09ccb96b71211eb535847ace8447f0c7c13
saxenaharsh <harsh.saxena@gmail.com>
2019-06-17 19:25:45 +0530

    Add files via upload

---
+++ Variables/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args){
+
+        int myFirstNumber = (15 + 5) + ( 2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+    }
+}
